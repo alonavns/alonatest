1288d3cc0b9603cd21cfdb337589a78701d8a719
alonavns <alona.vns@outlook.com>
2018-10-30 22:00:06 +0200

    heroku setup

--- alonacake/settings.py
+++ alonacake/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,3 +133,6 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     'compressor.finders.CompressorFinder',
 )
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
